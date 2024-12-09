0450adb97c644603d53b861d5c4df40cc1aacc8d
Quentin H. Reul <qreul@expert.ai>
2024-12-09 16:13:23 -0600

    Updated library to the latest dependencies

--- rqle_ai_langchain_util/llms/adapters/ollama_adapter.py
+++ rqle_ai_langchain_util/llms/adapters/ollama_adapter.py
@@ -3,9 +3,7 @@ from dotenv import load_dotenv
 
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain_community.llms.ollama import Ollama
-from langchain_community.chat_models import ChatOllama
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_ollama import OllamaLLM, ChatOllama, OllamaEmbeddings
 
 from rqle_ai_langchain_util.prompts.prompt_config import PromptConfig, PromptTypeEnum
 
@@ -17,7 +15,7 @@ def _load_ollama_llm_from_prompt_config(config: PromptConfig):
     :param config: the configuration for the LLM execution
     :return: a Langchain object configured for Ollama LLMs
     """
-    llm = Ollama(
+    llm = OllamaLLM(
         base_url=os.getenv('OLLAMA_ENDPOINT'),
         model=config.model_name,
         temperature=config.parameters.temperature,
